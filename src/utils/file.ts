f78c1fc60c2f3466a880a30c4a71770bf58339aa
Norbert Krajewski <norberttony@icloud.com>
2026-05-08 16:48:23 -0400

    moved utils to ts and removed unused code

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,18 +1,21 @@
+import fs, { PathLike } from "node:fs";
+import path from "node:path";
 
 // Utilities for managing files
 
-import fs from "node:fs";
-import path from "node:path";
+export interface DirStructure {
+    [k: string]: DirStructure | string;
+}
 
-export function isDirectory(path){
+export function isDirectory(path: PathLike): boolean {
     return fs.lstatSync(path).isDirectory();
 }
 
-export async function moveFile(oldPath, newPath){
+export async function moveFile(oldPath: PathLike, newPath: PathLike): Promise<void> {
     return new Promise((res, rej) => {
         fs.rename(oldPath, newPath, (err) => {
             if (err)
-                throw err;
+                rej(err);
             res();
         });
     });
@@ -21,7 +24,7 @@ export async function moveFile(oldPath, newPath){
 // structure is an object with keys being name of a directory or file. If the key is a directory
 // name, its value is another object (the same as a structure). If the key is a file, the value is
 // the contents of that file. These files are only initialized if they did not exist beforehand!
-export async function buildStructure(structure, root = "."){
+export async function buildStructure(structure: DirStructure, root: string = "."): Promise<void> {
     for (const [ k, v ] of Object.entries(structure)){
         const p = path.join(root, k);
         if (typeof v === "object"){
@@ -38,8 +41,8 @@ export async function buildStructure(structure, root = "."){
 
 // reads all of the files given and returns a list of content from each of those files.
 // if a read fails, returns undefined for that file.
-export async function readFiles(...paths){
-    const promises = [];
+export async function readFiles(...paths: PathLike[]): Promise<string[]> {
+    const promises: Promise<string>[] = [];
     for (const p of paths){
         promises.push(new Promise((res, rej) => {
             fs.readFile(p, (err, data) => {
@@ -55,9 +58,13 @@ export async function readFiles(...paths){
 
     const settled = await Promise.allSettled(promises);
 
-    const content = [];
-    for (const { value } of settled)
-        content.push(value);
+    const content: string[] = [];
+    for (const promise of settled){
+        if (promise.status == "fulfilled")
+            content.push(promise.value);
+        else
+            content.push("");
+    }
 
     return content;
 }
--- src/utils/input.ts
+++ src/utils/input.ts
@@ -1,24 +1,23 @@
-
 import readline from "node:readline";
 
 // Handles retrieving input from the user.
 
-const inputBuffer = [];
+const inputBuffer: string[] = [];
 
 const rli = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-rli.on("line", (data) => {
+rli.on("line", (data: string) => {
     inputBuffer.push(data);
 });
 
 // Reads in a single line of input from the user.
-export async function input(){
+export async function input(): Promise<string> {
     return new Promise((res, rej) => {
         if (inputBuffer.length > 0)
-            res(inputBuffer.shift());
+            res(inputBuffer.shift()!);
         else
             rli.question("", res);
     });
@@ -26,8 +25,8 @@ export async function input(){
 
 // Waits until the user inputs a number between min and max inclusive, in which case it returns
 // that number.
-export async function inputNumber(min, max){
-    let t = NaN;
+export async function inputNumber(min: number, max: number): Promise<number> {
+    let t: number = NaN;
     while (isNaN(t) || t < min || t > max)
         t = parseFloat(await input());
 
@@ -36,19 +35,19 @@ export async function inputNumber(min, max){
 
 // Presents a list of numbered choices to the user and waits until the user chooses one of the
 // options (by typing in a number representing that choice). Returns the number the user chose.
-export async function options(choices){
+export async function options(choices: string[]): Promise<number> {
     for (let i = 0; i < choices.length; i++)
         console.log(` ${i} - ${choices[i]}`);
     console.log("");
 
-    return new Promise(async (res, rej) => {
+    return new Promise(async (res, _) => {
         while (true){
             const answer = await input();
             const idx = choices.indexOf(answer);
             if (idx > -1){
                 res(idx);
                 break;
-            }else if (!isNaN(answer)){
+            }else if (!isNaN(parseInt(answer))){
                 const v = parseInt(answer);
                 if (v < 0 || v >= choices.length){
                     console.log(`The value ${v} is out of range.`);
